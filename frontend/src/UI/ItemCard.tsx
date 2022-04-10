dab61462033176c6a3c67d12444a5c9e92b0c5ef
Mathieu Keller <business@mathieu.tech>
2022-04-10 11:08:17 +0200

    fix jumping books on hover

--- frontend/src/UI/ItemCard.tsx
+++ frontend/src/UI/ItemCard.tsx
@@ -8,7 +8,7 @@ type ItemCardProps = {
 
 const ItemCard = ({cover, name, onClick}: ItemCardProps) => {
   return (
-    <div onClick={onClick} className="cursor-pointer m-5 p-3 flex hover:p-1 max-w-sm flex-col">
+    <div onClick={onClick} className="cursor-pointer m-5 p-3 flex hover:p-1 hover:my-3 max-w-sm flex-col">
       <img src={`data:image/jpeg;base64,${cover}`} alt={`cover picture of ${name}`}/>
       <h1 className="text-center break-words text-2xl font-bold">{name}</h1>
     </div>
