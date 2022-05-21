720be2279aed0d1e9c34c6a83b882d734ab07adf
Mathieu Keller <business@mathieu.tech>
2022-05-21 13:51:14 +0200

    lint

--- frontend/src/Api/Api.ts
+++ frontend/src/Api/Api.ts
@@ -2,8 +2,8 @@ const API_PREFIX = '/api';
 export const DOWNLOAD_API = (id: number) => `${API_PREFIX}/book/download/${id}`;
 export const DOWNLOAD_ORIGINAL_API = (id: number) => `${API_PREFIX}/book/original/download/${id}`;
 export const SEARCH_API = (search: string, page: number) => {
-  const params = new URLSearchParams({page: page.toString()});
-  if(search.trim() !== '') {
+  const params = new URLSearchParams({ page: page.toString() });
+  if (search.trim() !== '') {
     params.append('q', search);
   }
   return `${API_PREFIX}/book?${params.toString()}`;
