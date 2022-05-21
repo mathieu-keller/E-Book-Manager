93d4e73ee0ee9317f0bfd9cc1da9f01c21bde8d2
Mathieu Keller <business@mathieu.tech>
2022-05-21 19:19:19 +0200

    add padding again

--- frontend/src/UI/Badge.tsx
+++ frontend/src/UI/Badge.tsx
@@ -13,7 +13,7 @@ const Badge: Component<BadgeProps> = (props) => {
       onClick={props.onClick}
       class={'float-left border-2 w-max m-2 rounded-2xl bg-transparent dark:border-slate-200 dark:text-slate-50 border-slate-500 text-slate-800 font-semibold ' + hoverClasses}
     >
-      <div class="float-left pl-2 pt-2 pb-2 pr-1">
+      <div class="float-left p-2">
         {props.text}
       </div>
       <Show when={props.onRemove !== undefined}>
