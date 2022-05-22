987dade4b0b2f485827878cfa2ce7d211e55f6ec
Mathieu Keller <business@mathieu.tech>
2022-05-22 12:37:24 +0200

    change style

--- frontend/src/UI/Modal.tsx
+++ frontend/src/UI/Modal.tsx
@@ -13,7 +13,7 @@ const Modal: FlowComponent<ModalProps> = (props) => {
         <div onClick={props.onClose} class="fixed top-0 left-0 right-0 bottom-0 z-0 bg-gray-700/75 flex justify-center items-center"/>
         <div
           onClick={e => e.preventDefault()}
-          class="dark:bg-slate-900 p-5 dark:text-slate-300 bg-slate-50 text-slate-800 z-1 flex flex-col fixed  max-w-[80%] my-auto flex md:w-[30%] ">
+          class="dark:bg-slate-900 p-5 dark:text-slate-300 bg-slate-50 text-slate-800 z-1 flex flex-col fixed my-auto flex w-[100%] sm:w-[70%] md:w-[50%] xl:w-[30%]">
           <h1 class="border-b-2 w-full flex justify-center text-5xl font-bold pb-5">{props.title}</h1>
           {props.children}
         </div>
