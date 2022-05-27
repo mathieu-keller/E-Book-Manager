40d04090251e39ef79204002f2f4ae3da610e253
Mathieu Keller <business@mathieu.tech>
2022-05-27 12:10:59 +0200

    fix import

--- backend/resources/admin.go
+++ backend/resources/admin.go
@@ -12,7 +12,7 @@ import (
 func InitAdminApi(compress *gin.RouterGroup) {
 	defaultGroup := compress.Group("/admin")
 	defaultGroup.GET("/server/import", func(c *gin.Context) {
-		files, err := ioutil.ReadDir("/upload/")
+		files, err := ioutil.ReadDir("upload/upload")
 		if err != nil {
 			c.String(500, err.Error())
 			return
@@ -20,20 +20,20 @@ func InitAdminApi(compress *gin.RouterGroup) {
 		fileErrors := ""
 		for _, file := range files {
 			if !file.IsDir() {
-				f, err := os.Open(file.Name())
+				f, err := os.Open("upload/upload/" + file.Name())
 				if err != nil {
 					fileErrors += "Error: Book " + f.Name() + ": " + err.Error() + "\n"
 					f.Close()
 					continue
 				}
-				fileBytes := []byte{}
-				_, err = f.Read(fileBytes)
+				fileBytes := make([]byte, file.Size())
+				size, err := f.Read(fileBytes)
 				if err != nil {
 					fileErrors += "Error: Book " + f.Name() + ": " + err.Error() + "\n"
 					f.Close()
 					continue
 				}
-				zipReader, err := zip.NewReader(bytes.NewReader(fileBytes), file.Size())
+				zipReader, err := zip.NewReader(bytes.NewReader(fileBytes), int64(size))
 				if err != nil {
 					fileErrors += "Error: Book " + f.Name() + ": " + err.Error() + "\n"
 					f.Close()
@@ -48,6 +48,6 @@ func InitAdminApi(compress *gin.RouterGroup) {
 				f.Close()
 			}
 		}
-		c.String(200, "OK")
+		c.String(200, fileErrors)
 	})
 }
